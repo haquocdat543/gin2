83e6c27e9476bccb9f7d0936fe2fa2ecdcbd3069
haquocdat543 <wwwdatha543@gmail.com>
2025-07-02 16:05:38 +0700

    feat(api): add test2

--- main.go
+++ main.go
@@ -38,6 +38,34 @@ func main() {
 		)
 	}
 
+	test2 := r.Group("/test2")
+	{
+		test2.Handle(
+			"GET",
+			"/ping",
+			func(c *gin.Context) {
+				c.JSON(
+					http.StatusOK,
+					gin.H{
+						"message": "pong2",
+					},
+				)
+			},
+		)
+
+		test2.Handle(
+			"GET",
+			"/ask",
+			func(c *gin.Context) {
+				c.JSON(
+					http.StatusOK,
+					gin.H{
+						"message": "answer2",
+					},
+				)
+			},
+		)
+	}
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
-
