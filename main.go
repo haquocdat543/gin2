b5e3f73f9bac59344554be71e8f4d465b0ba142d
haquocdat543 <wwwdatha543@gmail.com>
2025-07-02 15:51:13 +0700

    feat: add gin get for ping

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Printf("Something")
+	r := gin.Default()
+
+	r.Handle(
+		"GET",
+		"/ping",
+		func(c *gin.Context) {
+			c.JSON(
+				http.StatusOK,
+				gin.H{
+					"message": "pong",
+				},
+			)
+		},
+	)
+
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
