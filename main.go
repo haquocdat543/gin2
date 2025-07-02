f1e3f9e08b0a8ab7720062ad658d0b0dc13adca0
haquocdat543 <wwwdatha543@gmail.com>
2025-07-02 16:03:34 +0700

    feat(api): test - add ask endpoint

--- main.go
+++ main.go
@@ -23,6 +23,19 @@ func main() {
 				)
 			},
 		)
+
+		test.Handle(
+			"GET",
+			"/ask",
+			func(c *gin.Context) {
+				c.JSON(
+					http.StatusOK,
+					gin.H{
+						"message": "answer",
+					},
+				)
+			},
+		)
 	}
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
