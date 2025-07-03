a8076eb7bafcce5b59895d28ada717ee499c1f5a
haquocdat543 <wwwdatha543@gmail.com>
2025-07-03 10:56:06 +0700

    feat(mod/user): dto - CreateUserDTO - Age - add [required]

--- src/module/user/dto.go
+++ src/module/user/dto.go
@@ -3,6 +3,6 @@ package user
 type CreateUserDTO struct {
 	Name  string `json:"name" binding:"required,min=3"`
 	Email string `json:"email" binding:"required,email"`
-	Age   int    `json:"age" binding:"gte=0,lte=130"`
+	Age   int    `json:"age" binding:"required,gte=0,lte=130"`
 }
 
