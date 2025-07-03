e5dccaf7c92303471a200a6179d35d5e60f6077f
haquocdat543 <wwwdatha543@gmail.com>
2025-07-03 16:50:25 +0700

    feat(db): remove auto migration

--- src/db/migrate.go
+++ src/db/migrate.go
@@ -1,13 +1,9 @@
 package db
 
 import (
-	"gin/src/module/user"
 	"gorm.io/gorm"
 )
 
 func Migrate(db *gorm.DB) error {
-	return db.AutoMigrate(
-		&user.User{},
-		// Add more models here as your app grows
-	)
+	return db.AutoMigrate()
 }
