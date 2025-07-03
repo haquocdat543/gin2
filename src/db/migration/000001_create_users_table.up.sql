56cd7a7311cde6af23b8461c2a4a76b3ba061303
haquocdat543 <wwwdatha543@gmail.com>
2025-07-03 16:59:13 +0700

    feat(migration): add [create_user_table]

---
+++ src/db/migration/000001_create_users_table.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE "users" (
+    id UUID PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    age INTEGER NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    deleted_at TIMESTAMPTZ
+);
+
+CREATE INDEX users_deleted_at_idx ON "users" (deleted_at);
+
