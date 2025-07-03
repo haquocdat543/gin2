935b8e7344f808590ad6d0b76bc38534285fd842
haquocdat543 <wwwdatha543@gmail.com>
2025-07-03 11:11:00 +0700

    feat(mod/user): entity - add uuid for id

--- src/module/user/entity.go
+++ src/module/user/entity.go
@@ -1,12 +1,25 @@
 package user
 
 import (
+	"time"
+
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type User struct {
-	gorm.Model
-	Name  string `json:"name"`
-	Email string `gorm:"uniqueIndex" json:"email"`
-	Age   uint   `json:"age"`
+	ID        uuid.UUID      `gorm:"type:uuid;primaryKey" json:"id"`
+	Name      string         `json:"name"`
+	Email     string         `gorm:"uniqueIndex" json:"email"`
+	Age       uint           `json:"age"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
+
+// Hook: Generate UUID before inserting
+func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
+	u.ID = uuid.New()
+	return
+}
+
