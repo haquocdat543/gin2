ab06220c3b5d44d589d0ce22e4122c122f4e8d37
haquocdat543 <wwwdatha543@gmail.com>
2025-07-04 12:34:21 +0700

    feat(share): add limit

---
+++ src/share/limit.go
@@ -0,0 +1,81 @@
+package share
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/ulule/limiter/v3"
+	"github.com/ulule/limiter/v3/drivers/store/memory"
+)
+
+// Define a GlobalRatelimit limit of 5 requests per minute for the specific API.
+var GlobalRatelimit = limiter.Rate{
+	Period: 20 * time.Second,
+	Limit:  5,
+}
+
+// RateLimitMiddleware creates a rate-limiting middleware for a specific rate.
+func RateLimitMiddleware(
+	rate limiter.Rate,
+) gin.HandlerFunc {
+	// Create a memory store for the rate limiter.
+	store := memory.NewStore()
+
+	// Create a new rate limiter instance.
+	instance := limiter.New(
+		store,
+		rate,
+	)
+
+	return func(
+		c *gin.Context,
+	) {
+		// Use the client's IP address as the key for rate limiting.
+		ip := c.ClientIP()
+
+		// Check the rate limit for the client.
+		context, err := instance.Get(
+			c,
+			ip,
+		)
+		if err != nil {
+			c.AbortWithStatusJSON(
+				http.StatusInternalServerError,
+				gin.H{
+					"error": "Internal Server Error",
+				},
+			)
+			return
+		}
+
+		// Set rate limit headers in the response.
+		c.Header(
+			"X-RateLimit-Limit",
+			"X-RateLimit-Limit",
+		)
+		c.Header(
+			"X-RateLimit-Remaining",
+			"X-RateLimit-Remaining",
+		)
+		c.Header(
+			"X-RateLimit-Reset",
+			"X-RateLimit-Reset",
+		)
+
+		// If the client has exceeded the rate limit, return a 429 Too Many Requests response.
+		if context.Reached {
+			c.AbortWithStatusJSON(
+				http.StatusTooManyRequests,
+				gin.H{
+					"error": "Too Many Requests",
+				},
+			)
+			return
+		}
+
+		// Continue to the next handler if the rate limit is not exceeded.
+		c.Next()
+	}
+}
