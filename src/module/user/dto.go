3d1f8c76d5ed758322cc9b85c6dbb92567ba4a9a
haquocdat543 <wwwdatha543@gmail.com>
2025-07-04 15:19:06 +0700

    feat(mod/user): dto - [CreateUserDTO] - add password field

--- src/module/user/dto.go
+++ src/module/user/dto.go
@@ -1,8 +1,8 @@
 package user
 
 type CreateUserDTO struct {
-	Name  string `json:"name" binding:"required,min=3"`
-	Email string `json:"email" binding:"required,email"`
-	Age   int    `json:"age" binding:"required,gte=0,lte=130"`
+	Name     string `json:"name" binding:"required,min=3"`
+	Password string `json:"password" binding:"required,min=8,max=20"`
+	Email    string `json:"email" binding:"required,email"`
+	Age      int    `json:"age" binding:"required,gte=0,lte=130"`
 }
-
