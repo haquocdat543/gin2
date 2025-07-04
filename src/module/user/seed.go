8cc57936e005d0c25e3a90f552d67ae4730e8234
haquocdat543 <wwwdatha543@gmail.com>
2025-07-04 15:33:14 +0700

    feat(mod/user): seed - add password

--- src/module/user/seed.go
+++ src/module/user/seed.go
@@ -4,26 +4,31 @@ var Seed = []*User{
 	{
 		Name:  "Jinzhu",
 		Email: "jinzhu@gmail.com",
+		Password: "12345678",
 		Age:   18,
 	},
 	{
 		Name:  "xiaoqinghua",
 		Email: "xiaoqinhua@gmail.com",
+		Password: "87654321",
 		Age:   19,
 	},
 	{
 		Name:  "lalala",
 		Email: "lalala@gmail.com",
+		Password: "lalala123",
 		Age:   21,
 	},
 	{
 		Name:  "lihanjiaqie",
 		Email: "lihanjiaqie@gmail.com",
+		Password: "lihanmeinu",
 		Age:   23,
 	},
 	{
 		Name:  "dengdengdengwo",
 		Email: "dengdengdengwo@gmail.com",
+		Password: "woaini<3",
 		Age:   24,
 	},
 }
