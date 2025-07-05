0d1690597c689ed222ceeb88ad528bab3129ac18
haquocdat543 <wwwdatha543@gmail.com>
2025-07-05 18:19:51 +0700

    feat(share/validate): improve functions

--- src/share/validate.go
+++ src/share/validate.go
@@ -8,8 +8,46 @@ import (
 
 // BindAndValidate binds the JSON body to the given DTO and validates it.
 // Returns false if there's an error, and sends the appropriate response.
-func BindAndValidate[T any](c *gin.Context, dto *T) bool {
+func BindJSONAndValidate[T any](c *gin.Context, dto *T) bool {
 	if err := c.ShouldBindJSON(dto); err != nil {
+		if ve, ok := err.(validator.ValidationErrors); ok {
+			errorsMap := make(map[string]string, len(ve))
+			for _, fe := range ve {
+				errorsMap[fe.Field()] = fmt.Sprintf(
+					"Field '%s' failed validation: tag='%s', param='%s'",
+					fe.Field(), fe.Tag(), fe.Param(),
+				)
+			}
+			c.JSON(400, gin.H{"errors": errorsMap})
+		} else {
+			c.JSON(400, gin.H{"error": err.Error()})
+		}
+		return false
+	}
+	return true
+}
+
+func BindQueryAndValidate[T any](c *gin.Context, dto *T) bool {
+	if err := c.ShouldBindQuery(dto); err != nil {
+		if ve, ok := err.(validator.ValidationErrors); ok {
+			errorsMap := make(map[string]string, len(ve))
+			for _, fe := range ve {
+				errorsMap[fe.Field()] = fmt.Sprintf(
+					"Field '%s' failed validation: tag='%s', param='%s'",
+					fe.Field(), fe.Tag(), fe.Param(),
+				)
+			}
+			c.JSON(400, gin.H{"errors": errorsMap})
+		} else {
+			c.JSON(400, gin.H{"error": err.Error()})
+		}
+		return false
+	}
+	return true
+}
+
+func BindUriAndValidate[T any](c *gin.Context, dto *T) bool {
+	if err := c.ShouldBindUri(dto); err != nil {
 		if ve, ok := err.(validator.ValidationErrors); ok {
 			errorsMap := make(map[string]string, len(ve))
 			for _, fe := range ve {
