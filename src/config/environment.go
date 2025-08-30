b7d1854a9ef7eedcf8f85dde5a55850799894f8c
haquocdat543 <wwwdatha543@gmail.com>
2025-08-30 10:35:36 +0700

    feat(config): environment - add jwt private key and public key

--- src/config/environment.go
+++ src/config/environment.go
@@ -1,17 +1,21 @@
 package config
 
 import (
-	"github.com/joho/godotenv"
+	"encoding/base64"
 	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 type EnvConfig struct {
-	DBHost     string
-	DBPort     string
-	DBUser     string
-	DBPassword string
-	DBName     string
+	DBHost        string
+	DBPort        string
+	DBUser        string
+	DBPassword    string
+	DBName        string
+	JWTPrivateKey string
+	JWTPublicKey  string
 }
 
 var ENV EnvConfig
@@ -23,12 +27,24 @@ func InitEnv() {
 		log.Println("Warning: .env file not found, using system environment variables")
 	}
 
+	// Decode jwt
+	JWTPrivateKey, err := base64.StdEncoding.DecodeString(os.Getenv("JWT_PRIVATE_KEY_BASE64_ENCODED"))
+	if err != nil {
+		panic(DECODE_JWT_PRIVATE_KEY_ERROR)
+	}
+	JWTPublicKey, err := base64.StdEncoding.DecodeString(os.Getenv("JWT_PUBLIC_KEY_BASE64_ENCODED"))
+	if err != nil {
+		panic(DECODE_JWT_PUBLIC_KEY_ERROR)
+	}
+
 	ENV = EnvConfig{
-		DBHost:     os.Getenv("DB_HOST"),
-		DBPort:     os.Getenv("DB_PORT"),
-		DBUser:     os.Getenv("DB_USER"),
-		DBPassword: os.Getenv("DB_PASSWORD"),
-		DBName:     os.Getenv("DB_NAME"),
+		DBHost:        os.Getenv("DB_HOST"),
+		DBPort:        os.Getenv("DB_PORT"),
+		DBUser:        os.Getenv("DB_USER"),
+		DBPassword:    os.Getenv("DB_PASSWORD"),
+		DBName:        os.Getenv("DB_NAME"),
+		JWTPrivateKey: string(JWTPrivateKey),
+		JWTPublicKey:  string(JWTPublicKey),
 	}
 
 }
---
+++ src/config/message.go
@@ -0,0 +1,6 @@
+package config
+
+const (
+	DECODE_JWT_PRIVATE_KEY_ERROR = "Decode jwt private key failed"
+	DECODE_JWT_PUBLIC_KEY_ERROR  = "Decode jwt public key failed"
+)
